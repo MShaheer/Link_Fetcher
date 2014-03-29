35e12add4bbd9a3c15ace2b2999528f078571d69
MShaheer <shaheernov123@gmail.com>
2014-03-29 22:46:22 +0500

    First Commit
    Just the project as it is.

---
+++ assignment_IAD/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace assignment_IAD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.GO = new System.Windows.Forms.Button();
+            this.Find_Links = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.url = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 78);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(314, 373);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(343, 174);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(233, 277);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // GO
+            // 
+            this.GO.Location = new System.Drawing.Point(414, 25);
+            this.GO.Name = "GO";
+            this.GO.Size = new System.Drawing.Size(75, 23);
+            this.GO.TabIndex = 2;
+            this.GO.Text = "GO";
+            this.GO.UseVisualStyleBackColor = true;
+            this.GO.Click += new System.EventHandler(this.GO_Click);
+            // 
+            // Find_Links
+            // 
+            this.Find_Links.Location = new System.Drawing.Point(414, 115);
+            this.Find_Links.Name = "Find_Links";
+            this.Find_Links.Size = new System.Drawing.Size(75, 23);
+            this.Find_Links.TabIndex = 3;
+            this.Find_Links.Text = "Links";
+            this.Find_Links.UseVisualStyleBackColor = true;
+            this.Find_Links.Click += new System.EventHandler(this.Find_Links_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(273, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // url
+            // 
+            this.url.AutoSize = true;
+            this.url.Location = new System.Drawing.Point(12, 30);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(18, 13);
+            this.url.TabIndex = 5;
+            this.url.Text = "url";
+            this.url.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 497);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Find_Links);
+            this.Controls.Add(this.GO);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button GO;
+        private System.Windows.Forms.Button Find_Links;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label url;
+    }
+}
+
---
+++ assignment_IAD/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_IAD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int curntloc = 0;
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static string FindLink(string htmlstr, ref int startloc)
+        {
+            int i;
+            int start, end;
+            string uri = null;
+
+
+            i = htmlstr.IndexOf("href=\"http", startloc, StringComparison.OrdinalIgnoreCase);
+            if (i != -1)
+            {
+                start = htmlstr.IndexOf('"', i) + 1;
+                end = htmlstr.IndexOf('"', start);
+                uri = htmlstr.Substring(start, end - start);
+                startloc = end;
+
+            }
+            return uri;
+        }
+
+
+
+
+        private void GO_Click(object sender, EventArgs e)
+        {
+
+       
+
+
+            int ch;
+            string html = null;
+            //Creating a webrequest to a uri
+            //  HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http:\\www.McGraw-Hill.com");
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(textBox1.Text);
+
+            //IWebProxy proxy = new WebProxy("10.1.0.11", 8080);
+            //proxy.Credentials = new NetworkCredential("test3", "karachi@3");
+            //req.Proxy = proxy;
+
+
+
+            //Next send that request and return response
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            //From the response, btain an input stream
+            Stream istrm = resp.GetResponseStream();
+
+            // Now consume that data 
+            for (int i = 1; ; i++)
+            {
+                ch = istrm.ReadByte();
+                if (ch == -1) break;
+                html += (char)ch;
+                //   richTextBox1.Text = html;
+            }
+
+            richTextBox1.Text = html;
+
+            // Close the Response. 
+            resp.Close();
+
+
+
+
+        }
+
+        private void Find_Links_Click(object sender, EventArgs e)
+        {
+            string link;
+            string htmltext = richTextBox1.Text;
+            string pagesource = htmltext;
+            do
+            {
+                link = FindLink(pagesource, ref curntloc);
+                richTextBox2.Text += FindLink(pagesource, ref curntloc);
+            } while (link != null);
+                   
+        }
+
+        }
+    }
+
